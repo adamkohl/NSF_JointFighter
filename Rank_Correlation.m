a0b8bd90d527aca2ffe55208e2326abcf2ee5e84
Adam Kohl <adamkohl@iastate.edu>
2015-12-15 16:55:54 -0600

    pdfs now stored in a struct
    Signed-off-by: Adam Kohl <adamkohl@iastate.edu>

--- Rank_Correlation.m
+++ Rank_Correlation.m
@@ -7,36 +7,47 @@ design_1 = [1 1 2 1 2 20 12 2 1 25 2 4 3 12 2 2 2 20000];
 design_tot = design_1;
 [n_designs,~] = size(design_tot);
 
+% Design attributes' upper and lower bounds 
+lb = [1 1 1 1 1 10 1 1 1 1 1 2 1 2 1 1 2 0];
+ub = [3 2 3 2 2 20 30 3 3 50 2 20 3 20 3 3 6 80000];
+intcon = [1 2 3 4 5 8 9 11 12 13 14 15 16 17];
+    
 for i = 1:n_designs
     design_iter = design_tot(i,:);
-    Type_LG = design_iter(1);
-    Tail_material = design_iter(2);
-    Type_tail = design_iter(3);
-    Wing_type = design_iter(4);
-    Spar_material = design_iter(5);
-    l_wing = design_iter(6);
-    l_chord = design_iter(7);
-    Rib_material = design_iter(8);
-    Skin_material = design_iter(9);
-    L_fuselage = design_iter(10);
-    Frame_material = design_iter(11);
-    n_frames = design_iter(12);
-    Longeron_material = design_iter(13);
-    n_longerons = design_iter(14);
-    Fuselage_skin_material = design_iter(15);
-    Type_engine = design_iter(16);
-    n_engines = design_iter(17);
-    Mass_payload = design_iter(18);
+    [~,nvars] = size(design_iter);
+%     Type_LG = design_iter(1);
+%     Tail_material = design_iter(2);
+%     Type_tail = design_iter(3);
+%     Wing_type = design_iter(4);
+%     Spar_material = design_iter(5);
+%     l_wing = design_iter(6);
+%     l_chord = design_iter(7);
+%     Rib_material = design_iter(8);
+%     Skin_material = design_iter(9);
+%     L_fuselage = design_iter(10);
+%     Frame_material = design_iter(11);
+%     n_frames = design_iter(12);
+%     Longeron_material = design_iter(13);
+%     n_longerons = design_iter(14);
+%     Fuselage_skin_material = design_iter(15);
+%     Type_engine = design_iter(16);
+%     n_engines = design_iter(17);
+%     Mass_payload = design_iter(18);
     % Stealth = design_iter(19); For government value output
-
-    % Design variables' upper and lower bounds 
-    lb = [1 1 1 1 1 10 1 1 1 1 1 2 1 2 1 1 2 0];
-    ub = [3 2 3 2 2 20 30 3 3 50 2 20 3 20 3 3 6 80000];
-    intcon = [1 2 3 4 5 8 9 11 12 13 14 15 16 17];
-    [nvars,~] = size(design_iter);
 
     % Value output for design alternative
     Aircraft_value = Value_function_3(design_iter);
+    
+    % Generate pdf's for attributes
+    for j=1:nvars
+        dx(j,:) = [lb(j):(ub(j)-lb(j))/1000:ub(j)];
+        pd(j) = makedist('Tri',lb(j),design_iter(j),ub(j));
+        vars_pdf(j,:) = pdf(pd(1,j),dx(j,:));
+    end
+    pdfs_design(i).graphimplem = vars_pdf;
+    pdfs_design(i).monteselect = pd;
+
+    
 end
 
 % Genetic algorithm from matlab optmization toolbox
@@ -52,8 +63,6 @@ end
 % Aircraft_value = Value_function(Type_LG,Tail_material, Type_tail,Wing_type,...
 %     Spar_material,l_wing,l_chord,Rib_material,Skin_material,L_fuselage,Frame_material,n_frames,...
 %     Longeron_material,n_longerons,Fuselage_skin_material,Type_engine,n_engines,Mass_payload)
-
-% Aircraft_value = Value_function(x);
 
 % L_fuselage = 20;
 % Frame_material = 1;
