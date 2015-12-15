f72ee7f9e2924497acc233d2b02f5432da74e7ae
Adam Kohl <adamkohl@iastate.edu>
2015-12-15 16:13:52 -0600

    Started looping implementation
    Signed-off-by: Adam Kohl <adamkohl@iastate.edu>

--- Rank_Correlation.m
+++ Rank_Correlation.m
@@ -2,40 +2,42 @@
 % Establishes a rank correlation for design alternatives
 clc;clear;
 format longEng
-
 % Design alternative for testing
-Type_LG = 1;
-Tail_material = 1;
-Type_tail =2;
-Wing_type =1;
-Spar_material = 2;
-l_wing = 20;
-l_chord = 12;
-Rib_material = 2;
-Skin_material = 1;
-L_fuselage = 25;
-Frame_material = 2;
-n_frames = 4;
-Longeron_material = 3;
-n_longerons = 12;
-Fuselage_skin_material = 2;
-Type_engine = 2;
-n_engines = 2;
-Mass_payload = 20000;
-% Stealth = 0.5; For government value
-design1 = [Type_LG,Tail_material,Type_tail,Wing_type,Spar_material,l_wing,l_chord,...
-    Rib_material,Skin_material,L_fuselage,Frame_material,n_frames,Longeron_material,...
-    n_longerons,Fuselage_skin_material,Type_engine,n_engines,Mass_payload];
+design_1 = [1 1 2 1 2 20 12 2 1 25 2 4 3 12 2 2 2 20000];
+design_tot = design_1;
+[n_designs,~] = size(design_tot);
 
-% Design variables' upper and lower bounds 
-lb = [1 1 1 1 1 10 1 1 1 1 1 2 1 2 1 1 2 0];
-ub = [3 2 3 2 2 20 30 3 3 50 2 20 3 20 3 3 6 80000];
-intcon = [1 2 3 4 5 8 9 11 12 13 14 15 16 17];
-nvars = 18;
+for i = 1:n_designs
+    design_iter = design_tot(i,:);
+    Type_LG = design_iter(1);
+    Tail_material = design_iter(2);
+    Type_tail = design_iter(3);
+    Wing_type = design_iter(4);
+    Spar_material = design_iter(5);
+    l_wing = design_iter(6);
+    l_chord = design_iter(7);
+    Rib_material = design_iter(8);
+    Skin_material = design_iter(9);
+    L_fuselage = design_iter(10);
+    Frame_material = design_iter(11);
+    n_frames = design_iter(12);
+    Longeron_material = design_iter(13);
+    n_longerons = design_iter(14);
+    Fuselage_skin_material = design_iter(15);
+    Type_engine = design_iter(16);
+    n_engines = design_iter(17);
+    Mass_payload = design_iter(18);
+    % Stealth = design_iter(19); For government value output
 
-% Value output for design alternative
-Aircraft_value = Value_function_3(design1);
+    % Design variables' upper and lower bounds 
+    lb = [1 1 1 1 1 10 1 1 1 1 1 2 1 2 1 1 2 0];
+    ub = [3 2 3 2 2 20 30 3 3 50 2 20 3 20 3 3 6 80000];
+    intcon = [1 2 3 4 5 8 9 11 12 13 14 15 16 17];
+    [nvars,~] = size(design_iter);
 
+    % Value output for design alternative
+    Aircraft_value = Value_function_3(design_iter);
+end
 
 % Genetic algorithm from matlab optmization toolbox
 %[x,fval,exitflag,output,population,score] = ga_aircraft(nvars,lb,ub,intcon)
